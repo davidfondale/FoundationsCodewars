32acde56f8daea97fbd433cc895924e06434039f
David Fondale <davidfondale@gmail.com>
2025-08-28 21:15:52 -0400

    commit all

---
+++ Codewars.py
@@ -0,0 +1,25 @@
+#Even or Odd
+
+def odd_even(x):
+    if x % 2 == 0:
+        return "even";
+    else:
+        return "odd";
+
+#Convert to String
+
+def num_string(x):
+    str = str(x);
+    return x;
+
+#Remove Spaces
+
+def no_space(x):
+    x_str = str(x);
+    clean_x = x.strip();
+    new_str = "";
+    for char in clean_x:
+        if char == " ":
+            continue;
+        new_str = new_str + char;
+    return new_str;
